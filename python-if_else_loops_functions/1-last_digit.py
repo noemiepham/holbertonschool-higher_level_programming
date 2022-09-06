2d081b053356a0e5e180531aa5c3a170457c985e
noemiepham <huynhtieuquy@gmail.com>
2022-09-06 16:53:37 +0200

    Number is negative return the last number negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,10 @@
 from cmath import pi
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+last = abs(number) % 10
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
     print(f"Last digit of {number} is 0 and is 0")
 else:
-    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {-last} and is less than 6 and not 0")
