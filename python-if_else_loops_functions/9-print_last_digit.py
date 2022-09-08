13d5db9de56da3d9b220af3fd6291a0049a7720e
noemiepham <huynhtieuquy@gmail.com>
2022-09-08 14:58:46 +0200

    print nulber

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     print((abs(number) % 10), end="")
-    if number < 0:
-        last = abs(number) % 10
-        return (last)
+    last = abs(number) % 10
+    return (last)
