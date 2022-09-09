2967ec5ab70fd88214a31222609531954b7ef971
noemiepham <huynhtieuquy@gmail.com>
2022-09-09 11:31:32 +0200

    Argv de python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,12 @@
 import sys
 if __name__ == "__main__":
     i = len(sys.argv)
-if i < 2:
-    print("0 arguments.")
-elif i == 2:
-    print("1: argument:")
-    print("{}: {}".format(j, sys.argv[j]))
-
-else:
-    print("{} arguments:".format(i - 1))
-for j in range(1, i):
-    print("{}: {}".format(j, sys.argv[j]))
+    if i < 2:
+        print("0 arguments.")
+    elif i == 2:
+        print("1: argument:")
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(i - 1))
+        for j in range(1, i):
+            print("{}: {}".format(j, sys.argv[j]))
