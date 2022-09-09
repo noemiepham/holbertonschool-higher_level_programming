9df5d7aca208d5423d707432780e895e9395f4dd
noemiepham <huynhtieuquy@gmail.com>
2022-09-09 13:49:35 +0200

    Change method to print * way

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,20 +3,25 @@ import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     numArgv = len(sys.argv)
+    a = int(sys.argv[1])
+    sign = sys.argv[2]
+    b = int(sys.argv[3])
     if numArgv != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit("1")
     else:
-        if sys.argv[2] == '+':
-            calcul = add(int(sys.argv[1]), int(sys.argv[3]))
-        elif sys.argv[2] == '-':
-            calcul = sub(int(sys.argv[1]), int(sys.argv[3]))
-        elif sys.argv[2] == "*":
-            calcul = mul(int(sys.argv[1]), int(sys.argv[3]))
-        elif sys.argv[2] == '/':
-            calcul = div(int(sys.argv[1]), int(sys.argv[3]))
+        if sign == '+':
+            calcul = add(a, b)
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif sign == '-':
+            calcul = sub(a, b)
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif sign == "*":
+            calcul = mul(a, b)
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif sign == '/':
+            calcul = div(a, b)
+            print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit("1")
-        print("{} {} {} = {}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3], calcul))
