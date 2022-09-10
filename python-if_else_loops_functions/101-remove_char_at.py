f244d9e03b96978b040da8828bc4f86360e7fef7
noemiepham <huynhtieuquy@gmail.com>
2022-09-10 07:13:24 +0200

    Remove charactere

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+from tkinter import N
+
+
 def remove_char_at(str, n):
     lenStr = len(str)
+    newstr = ""
     if lenStr <= n or n < 0:
         return str
     else:
         for i in range(lenStr):
-            return str[:n-1] + str[n:]
+            if i != n:
+                newstr = newstr + str[i]
+        return newstr
