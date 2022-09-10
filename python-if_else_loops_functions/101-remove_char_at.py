d6f7ab48c8d1db35200d8b9e4a49a53378674585
noemiepham <huynhtieuquy@gmail.com>
2022-09-10 06:56:40 +0200

    Remove character in the string

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from os import remove
+
+
+def remove_char_at(str, n):
+    lenStr = len(str)
+    if lenStr <= n or n < 0:
+        return str
+    else:
+        for i in range(lenStr):
+            return str[:n-1] + str[n:]
