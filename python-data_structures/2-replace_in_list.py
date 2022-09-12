8f55a0e77fa687e64d4ffe3c3d5558895190ead1
noemiepham <huynhtieuquy@gmail.com>
2022-09-12 14:30:28 +0200

    Return old list if idx negative

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return
+        return my_list
     elif idx > len(my_list):
-        return
+        return my_list
     else:
         for i in range(len(my_list)):
             if i == idx:
