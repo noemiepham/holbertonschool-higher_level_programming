41d3c48e78d13134ae179e1dacab879e0df92c3e
noemiepham <huynhtieuquy@gmail.com>
2022-09-12 14:20:47 +0200

    Return NONE

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return 0
+        return
     elif idx > len(my_list):
-        return 0
+        return
     else:
-     for i in range(len(my_list)):
-        if i == idx:
-            return my_list[i]
+        for i in range(len(my_list)):
+            if i == idx:
+                return my_list[i]
