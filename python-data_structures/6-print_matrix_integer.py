6964059210d485d512dcb410450a7ee75927fc8b
noemiepham <huynhtieuquy@gmail.com>
2022-09-13 10:09:47 +0200

    add condition Matrix is empty

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        array = matrix[row]
-        lenArray = len(array)
-        for idx in range(lenArray):
-            if idx != lenArray - 1 and lenArray == 0:
-                print("{:d}".format(array[idx]), end=" ")
-            else:
-                print("{:d}".format(array[idx]))
+    if matrix != []:
+        for row in range(len(matrix)):
+            array = matrix[row]
+            lenArray = len(array)
+            for idx in range(lenArray):
+                if idx != lenArray - 1:
+                    print("{:d}".format(array[idx]), end=" ")
+                else:
+                    print("{:d}".format(array[idx]))
+    else:
+        print()
