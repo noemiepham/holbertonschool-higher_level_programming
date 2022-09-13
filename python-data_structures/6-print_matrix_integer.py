7398c1d2de4bb7a95ea04368bf1456031de3987c
noemiepham <huynhtieuquy@gmail.com>
2022-09-13 10:00:47 +0200

    Remove the last space in array

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    y = 0
-    for row in matrix:
-        for i in row: 
-            if i == row:
-               print("{:d}".format(i))
-            print("{:d}".format(i), end=" ")
-        print()
-
-
-    # for row in range(len(matrix)):
-    #      newlist = matrix[row]
-    #      for i in range(len(newlist)):
-
-    #          print("{}".format(newlist[i]), end="")
-    #      print()
+    for row in range(len(matrix)):
+        array = matrix[row]
+        lenArray = len(array)
+        for idx in range(lenArray):
+            if idx != lenArray - 1:
+                print("{}".format(array[idx]), end=" ")
+            else:
+                print("{}".format(array[idx]))
