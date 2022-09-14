a0f74dab549a84aa4496f78ac67b8b91ae860aa4
noemiepham <huynhtieuquy@gmail.com>
2022-09-14 07:14:03 +0200

    Add condition Max is the last element

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,5 +4,7 @@ def max_integer(my_list=[]):
         return None
     for idx in range(len(my_list)):
         max = my_list[idx]
-        if max > my_list[idx + 1]:
+        if idx == len(my_list) - 1:
+            return max
+        elif max > my_list[idx + 1]:
             return max
