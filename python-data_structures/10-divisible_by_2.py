2eceadb61c2cd03ae7c8dde6f55e508bafdf541e
noemiepham <huynhtieuquy@gmail.com>
2022-09-14 09:33:15 +0200

    Divisible value at indx

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    newlist = []
     for i in my_list:
-        newlist = [i for i in my_list  if i % 2 == 0 ]
-        return newlist
+        if i % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+    return newlist
