2f8291a26e3bb81c58458cb2f1a6927192cd8e87
noemiepham <huynhtieuquy@gmail.com>
2022-09-14 06:59:39 +0200

    Change condition  tuple is empty

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        sentence[0] = None
     length = len(sentence)
+    if sentence == "":
+        return (length, None)
     first = sentence[0]
     return (length, first)
