820f5c69dfc62f577d6bbdaffc2d966e66f053ef
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 05:52:47 +0200

    Add condition if no common element

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+
     if set_1 & set_2:
         common = set_1 & set_2
         return common
+    else:
+        return []
 
     # method 2
     # common = i for i in set_1 if i in set_b
