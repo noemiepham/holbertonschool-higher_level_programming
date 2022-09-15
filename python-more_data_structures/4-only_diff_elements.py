c0fd6e858aa8a65aa6a72293d5b63864299f53ca
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 10:30:43 +0200

    Print difference element two set

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     myset = {*set_1, * set_2}
-    return myset
+    common = [i for i in set_1 if i in set_2]
+    diff = myset - set(common)
+    return diff
