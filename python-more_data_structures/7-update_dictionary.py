f2983a1152b0754dfae42f0dcdf019b2ab04ad19
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 14:23:37 +0200

    change code style

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-    
+
     # Method 2:
     #  newdict = {key: value}
     # a_dictionary.update(newdict)
