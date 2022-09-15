d9e3df06c859ec912ecb4fbe5ed13e8a7141de51
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 04:20:58 +0200

    Searche and replace tow way

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newlist = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-        newlist.append(my_list[i])
-    return newlist
+    newlist = [element for element in my_list]
+    for item in newlist:
+        if newlist[item] == search:
+            newlist[item] = replace
+        return newlist
+
+    # method2: newlist = [replace
+    # if item == search else item for item in my_list ]
+    # return newlist
