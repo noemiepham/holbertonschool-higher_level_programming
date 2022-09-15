6b1888ae039693c0e333754afc4381975a704de6
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 16:02:26 +0200

    Mul with number

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    mul = list(map(lambda x: x * number, my_list))
-    return mul
+    mul = (lambda x: x * number)
+    total = list(map(mul, my_list))
+    return total
