7a06fb03d155b3c8ab0dce46c880ff4ee54512dc
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 14:20:02 +0200

    Add bin pash

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+    
     # Method 2:
     #  newdict = {key: value}
     # a_dictionary.update(newdict)
     # return a_dictionary
-
     # for key, value in a_dictionary.items():
-    a_dictionary[key] = value
-    return a_dictionary
