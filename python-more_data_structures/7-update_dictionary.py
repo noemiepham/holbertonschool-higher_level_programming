9cac61f99fa85f03728f19181cb05336c260420c
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 14:16:36 +0200

    update condition stdrr

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -5,6 +5,6 @@ def update_dictionary(a_dictionary, key, value):
     # a_dictionary.update(newdict)
     # return a_dictionary
 
-    for key, value in a_dictionary.items():
-        a_dictionary[key] = value
-        return (a_dictionary)
+    # for key, value in a_dictionary.items():
+    a_dictionary[key] = value
+    return a_dictionary
