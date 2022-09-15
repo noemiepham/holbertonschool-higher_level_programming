6f5af6cc5582baec0732b1236df5551bcc211ff5
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 04:32:12 +0200

    Search py comprehension way

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newlist = [element for element in my_list]
-    for item in newlist:
-        if newlist[item] == search:
-            newlist[item] = replace
-        return newlist
+    # newlist = [element for element in my_list]
+    # for item in newlist:
+    #    if item == search:
+    #       item = replace
+    #   return newlist
 
-    # method2: newlist = [replace
-    # if item == search else item for item in my_list ]
-    # return newlist
+    newlist = [replace if item == search else item for item in my_list]
+    return newlist
