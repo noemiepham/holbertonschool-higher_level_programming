9a7f8773779df94382f1844f174d276db11bdc0a
noemiepham <huynhtieuquy@gmail.com>
2022-09-15 04:59:35 +0200

    Set uniq item list

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq = set(my_list)
+    total = sum(my_list)
+    return sum
+
+    # method 2
+    #    sum = 0
+    #    uniq = set(my_list)
+    #    for num in uniq:
+    #       sum += num
+    #    return sum
