e9935bb6a283cf24812ee0b70ec1d54cc8736ba5
noemiepham <huynhtieuquy@gmail.com>
2022-09-17 07:53:11 +0200

    Print x ecept Erro

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,12 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     if x == 0 or my_list == []:
         return (0)
-    for i in my_list[:x]:
-        try:
-            print("{}".format(i), end="")
-        except TypeError:
-            print(my_list)
-        except UnboundLocalError:
-            print(0)
-    print()
-    return (i)
+    else:
+        for i in my_list[:x]:
+            try:
+                print("{}".format(i), end="")
+            except TypeError:
+                print(my_list)
+            except UnboundLocalError:
+                print(0)
+        print()
+        return (i)
