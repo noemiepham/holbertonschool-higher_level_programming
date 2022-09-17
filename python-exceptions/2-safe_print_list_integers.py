9987e20add1cfa7d7f2e24bce8df0a3f6c7d629d
noemiepham <huynhtieuquy@gmail.com>
2022-09-17 13:34:41 +0200

    print integer pass str

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+from distutils.log import error
+from itertools import count
+
+
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        while count < x:
-            print("{}".format(my_list[count]), end="")
+    count = 0
+    error = 0
+    while count < x:
+        try:
+            print("{:d}".format(my_list[count]), end="")
             count += 1
-    except ValueError:
-        pass
+        except (ValueError, TypeError):
+            count += 1
+            error += 1
+    count -= error
     print()
-    return (count)
+    return count
