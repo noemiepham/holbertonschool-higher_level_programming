7b6b0290e833aac89213e20fbd693da7f6912ce3
noemiepham <huynhtieuquy@gmail.com>
2022-09-17 09:38:38 +0200

    Used method try and except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(int(x)):
-            print("{}".format(my_list[i]), end="")
-            i += 1
-        print()
-        return (i)
+          count = 0
+          while count < x:
+            print("{}".format(my_list[count]), end="")
+            count += 1
+          print()
+          return (count)
     except IndexError:
         print()
-        return (i)
+        return (count)
         pass
 
 # if x == 0 or my_list == []:
