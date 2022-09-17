a2b56683d8b20a228991a753e0bfb28a91302efa
noemiepham <huynhtieuquy@gmail.com>
2022-09-17 09:33:30 +0200

    Used method try and except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+from itertools import count
+
+
 def safe_print_list(my_list=[], x=0):
-    if x == 0 or my_list == []:
-        return (0)
-    else:
-        for i in my_list[:x]:
-            try:
-                print("{}".format(i), end="")
-            except TypeError:
-                print(my_list)
-            except UnboundLocalError:
-                print(0)
+    try:
+        for i in range(int(x)):
+            print("{}".format(my_list[i]), end="")
+            i += 1
         print()
         return (i)
+    except IndexError:
+        print()
+        return (i)
+        pass
+
+# if x == 0 or my_list == []:
+#    return (0)
+# else:
+#    for i in my_list[:x]:
+#          print("{}".format(i), end="")
+#   print()
+#   return (i)
