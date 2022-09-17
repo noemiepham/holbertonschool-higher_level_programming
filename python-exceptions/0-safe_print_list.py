f735ae708a4b4c57da3ed2fc1d515fae92f6affc
noemiepham <huynhtieuquy@gmail.com>
2022-09-17 07:47:46 +0200

    Print x ecept Erro

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in my_list[:x]:
+        try:
+            print("{}".format(i), end="")
+        except TypeError:
+            print(my_list)
+    print()
+    return (i)
