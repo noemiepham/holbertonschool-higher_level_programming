e5d480d448df021de02c42c05c2ff4813af867ce
noemiepham <huynhtieuquy@gmail.com>
2022-09-18 19:34:08 +0200

    Magic calculation

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from logging import exception
+from unittest import result
+
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = a + b
+            break
+    return result
