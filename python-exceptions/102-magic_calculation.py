85686120f92ea735e5d70be12ec4d369d6391cce
noemiepham <huynhtieuquy@gmail.com>
2022-09-18 19:40:03 +0200

    Pyte code

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -8,10 +8,10 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise exception('Too far')
+                raise exception("Too far")
             else:
-                result += a ** b / i
+                result += (a ** b) / i
         except:
-            result = a + b
+            result = b + a
             break
     return result
