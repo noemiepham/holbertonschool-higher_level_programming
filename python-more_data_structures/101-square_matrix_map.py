3e22a0985fa1f9ce3dc9e1c00e8cbc3326e1e0f4
noemiepham <huynhtieuquy@gmail.com>
2022-09-18 22:48:40 +0200

    add Square matrix

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    newarray = []
-    for row in matrix:
-        squre_itertor = map(lambda i: i ** 2, row)
-
-        newarray.append(list(squre_itertor))
-    return newarray
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
