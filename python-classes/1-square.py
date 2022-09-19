4c4e18a5ed98ca22c88ac8816f938a6cef632e81
noemiepham <huynhtieuquy@gmail.com>
2022-09-19 15:38:43 +0200

    Private Atribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' python3 -c 'print(__import__("my_module").__doc__)'''
+
+
+class Square():
+    '''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
+
+    def __init__(self, size):
+        self.__size = size
+    '''python3 -c 'print(__import__("my_module").
+    MyClass.my_function.__doc__)'''
