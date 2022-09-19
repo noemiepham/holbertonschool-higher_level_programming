d68c6fecc3ad24916801afb685179816272ec08e
noemiepham <huynhtieuquy@gmail.com>
2022-09-19 12:09:34 +0200

    Add module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+print(__import__("my_module").__doc__)
+print(__import__("my_module").MyClass.__doc__)
+print(__import__("my_module").my_function.__doc__)
+print(__import__("my_module").MyClass.my_function.__doc__)
+
+
 class Square:
     pass
