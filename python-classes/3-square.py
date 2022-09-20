3ce2a5be6977392ffa25d447cbb0852d94fd0ead
noemiepham <huynhtieuquy@gmail.com>
2022-09-20 06:14:50 +0200

    Square number

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' python3 -c 'print(__import__("my_module").__doc__)'''
+
+
+from turtle import clear
+
+
+class Square():
+    '''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
+
+    def __init__(self, size=0):
+        self.__size = size
+        '''python3 -c 'print(__import__("my_module").
+          MyClass.my_function.__doc__)'''
+        try:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            if not isinstance(size, int):
+                raise TypeError('size must be an integer')
+        except TypeError:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        return (self.__size * self.__size)
