801a22be0e5a1eb91b9940ff0ce6182a749e0ad0
noemiepham <huynhtieuquy@gmail.com>
2022-09-20 22:10:43 +0200

    Codition tuple

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+'''python3 -c 'print(__import__("my_module").__doc__)'''
 class Square():
+    '''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
     def __init__(self, size=0, position=(0, 0)):
+        '''python3 -c 'print(__import__("my_module").my_function.__doc__)'''
+        '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
         self.size = size
         self.position = position
 
