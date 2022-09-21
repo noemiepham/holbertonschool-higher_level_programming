8dbc6fb18903c5d1e42805f5d6df7cc6d29a742a
noemiepham <huynhtieuquy@gmail.com>
2022-09-21 12:27:27 +0200

    Condition wit type a and b

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,14 +2,21 @@
 '''python3 -c 'print(__import__("my_module").__doc__)'''
 
 
+from tkinter import filedialog
+
+
 def add_integer(a, b=98):
-     try:
-          return (int(a) + int(b))
-     except:
-          if type(a) != int:
-               raise TypeError('a must be an integer')
-          if type(b) != int:
-               raise TypeError('b must be an interger')
+    '''python3 -c 'print(__import__("my_module").my_function.__doc__)'''
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+    if type(a) != int:
+        raise TypeError('a must be integer')
+    elif type(b) != int:
+        raise TypeError('b must be integer')
+    else:
+        return (a + b)
+
 
 if __name__ == "__main__":
     import doctest
