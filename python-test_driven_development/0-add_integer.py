5d898a7b0dc41d76d58205ce2670ca0a7f7dcb24
noemiepham <huynhtieuquy@gmail.com>
2022-09-21 12:29:47 +0200

    Coriger bytecodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,16 +2,15 @@
 '''python3 -c 'print(__import__("my_module").__doc__)'''
 
 
-
 def add_integer(a, b=98):
     '''python3 -c 'print(__import__("my_module").my_function.__doc__)'''
     if type(a) == float or type(b) == float:
         a = int(a)
         b = int(b)
     if type(a) != int:
-        raise TypeError('a must be integer')
+        raise TypeError('a must be an integer')
     elif type(b) != int:
-        raise TypeError('b must be integer')
+        raise TypeError('b must be an integer')
     else:
         return (a + b)
 
