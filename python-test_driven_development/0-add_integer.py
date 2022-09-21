68535d873ecd132b4569a33291ef990e9411c110
noemiepham <huynhtieuquy@gmail.com>
2022-09-21 12:31:14 +0200

    Coriger bytecodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,8 +13,3 @@ def add_integer(a, b=98):
         raise TypeError('b must be an integer')
     else:
         return (a + b)
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
