119062ff498db0068895f2bfc2a391e90b1858b4
noemiepham <huynhtieuquy@gmail.com>
2022-09-22 16:33:32 +0200

    Cut text with : ? .

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" function that prints a text with
+ 2 new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """ text must be a string,
+    otherwise raise a TypeError exception with the message
+    text must be a string """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text) - 1):
+        if text[i - 1] == "." or text[i - 1] == '?' or text[i - 1] == ":":
+            print()
+        else:
+            print(text[i], end="")
+    print()
