fa6cc4a2a836d5173f9618f4d6c238df20e0f291
noemiepham <huynhtieuquy@gmail.com>
2022-09-23 07:39:51 +0200

    Add cases unittest

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,32 +4,30 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-     """Test for “max at the end” list order"""
+    """Test for “max at the end” list order"""
 
-     def text_max_integer(self):
-          """Test list number normal and order"""
-          self.assertEqual([1, 2, 3, 4], 4)
+    def text_max_integer(self):
+        """Test list number normal and order"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-     def text_begining_inorder(self):
-          """ Test list number normal and inoder"""
-          self.assertEquals([4, 2, 1, 3], 4)
-     
-     def text_max_inmiddle_list(self):
-          """ Test for “max in the middle” exists"""
-          self.assertEquals([1,2,4,3], 4)
-     
-     def text_only_negative(self):
-          """Test for “only negative numbers in the list” exists"""
-          self.assertEquals([-1, -2, -3, -4], -1 )
+    def text_begining_inorder(self):
+        """ Test list number normal and inoder"""
+        self.assertEquals(max_integer([4, 2, 1, 3]), 4)
 
-     def text_only_one_element(self):
-          """Test for “list of one element” exists"""
-          self.assertEquals([1], 1)
+    def text_max_inmiddle_list(self):
+        """ Test for “max in the middle” exists"""
+        self.assertEquals(max_integer([1, 2, 4, 3]), 4)
 
-     def text_list_empty(self):
-          """List is empty"""
-          self.assertEquals([], None)
+    def text_only_negative(self):
+        """Test for “only negative numbers in the list” exists"""
+        self.assertEquals(max_integer([-1, -2, -3, -4]), -1)
 
-     
-     
+    def text_only_one_element(self):
+        """Test for “list of one element” exists"""
+        self.assertEquals(max_integer([1]), 1)
+
+    def text_list_empty(self):
+        """List is empty"""
+        self.assertEquals(max_integer([]), None)
