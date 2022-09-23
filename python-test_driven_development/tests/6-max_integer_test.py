4f7ef4cb120113488a6c9ecaa373537c7db8f0eb
noemiepham <huynhtieuquy@gmail.com>
2022-09-23 07:41:21 +0200

    Add cases unittest

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -14,20 +14,20 @@ class TestMaxInteger(unittest.TestCase):
 
     def text_begining_inorder(self):
         """ Test list number normal and inoder"""
-        self.assertEquals(max_integer([4, 2, 1, 3]), 4)
+        self.assertEqual(max_integer([4, 2, 1, 3]), 4)
 
     def text_max_inmiddle_list(self):
         """ Test for “max in the middle” exists"""
-        self.assertEquals(max_integer([1, 2, 4, 3]), 4)
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
 
     def text_only_negative(self):
         """Test for “only negative numbers in the list” exists"""
-        self.assertEquals(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def text_only_one_element(self):
         """Test for “list of one element” exists"""
-        self.assertEquals(max_integer([1]), 1)
+        self.assertEqual(max_integer([1]), 1)
 
     def text_list_empty(self):
         """List is empty"""
-        self.assertEquals(max_integer([]), None)
+        self.assertEqual(max_integer([]), None)
