f06fbb11109350bb3432889d93fc4aed6404fec1
noemiepham <huynhtieuquy@gmail.com>
2022-09-23 07:26:20 +0200

    Unitt test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,4 +5,5 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-     def test_
+     def text_list_is_not_integer(self):
+          self.assertTrue()
