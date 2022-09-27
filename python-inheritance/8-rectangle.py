afa59e0459d4c5aab5b788ed8950af471d0cc3db
noemiepham <huynhtieuquy@gmail.com>
2022-09-27 22:48:54 +0200

    PEP8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,8 +3,10 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """ Rectangle"""
+
     def __init__(self, width, height):
         """constructor"""
         super().integer_validator("width", width)
