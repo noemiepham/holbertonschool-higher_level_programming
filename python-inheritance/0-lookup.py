006576cecb9556aca69b524e3ab9ab61de607c49
noemiepham <huynhtieuquy@gmail.com>
2022-09-27 15:51:00 +0200

    test code print sorted

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""look up"""
+
+
+def lookup(obj):
+    """ Function that returns the list of avail. attributes and methods
+    of an object.
+    """
+    return (dir(obj))
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Mylist"""
+
+
+class MyList(list):
+    """ Contains list"""
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        return (sorted(self))
