3301aaa77fdb18cbae5bc92dcc3da03b714d4655
noemiepham <huynhtieuquy@gmail.com>
2022-09-27 18:28:22 +0200

    Add condition inherited

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,6 +5,6 @@
 def inherits_from(obj, a_class):
     """check obj is an instanc a class that inherited"""
     if type(obj) != a_class:
-        return True
+        return (isinstance(obj, a_class))
     else:
         return False
