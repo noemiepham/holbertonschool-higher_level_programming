48b55449a5a998b9340192f9c83237a3420db46a
noemiepham <huynhtieuquy@gmail.com>
2022-09-27 21:58:16 +0200

    Inherits method

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Rectangle"""
+from pydoc import classname
+from turtle import width
+
+
+class BaseGeometry:
+    """class empty"""
+
+    def area(self):
+        """Raise Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Check type de value input"""
+        self.name = name
+        self.value = value
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        """constructor"""
+        BaseGeometry.__init__(self)
+        bg = BaseGeometry()
+        bg.integer_validator(width, height)
