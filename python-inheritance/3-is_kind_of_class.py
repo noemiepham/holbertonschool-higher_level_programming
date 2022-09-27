b70f5fdee62aa8299c7f1e3f521fec61046c1bae
noemiepham <huynhtieuquy@gmail.com>
2022-09-27 18:01:35 +0200

    Check is an instance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+     """Check the object is an instance of a class that inherited from """
+     return (isinstance(obj, a_class))
