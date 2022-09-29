963eb421d2f5467fe2070fddb0544fbf5b17f0e4
noemiepham <huynhtieuquy@gmail.com>
2022-09-29 14:58:15 +0200

    decode->str, code to json, write append

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""import module json"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+  """writes an Obj to a text, using JSON"""
+  with open(filename, "a") as my_file:
+    json_object = json.dumps(my_obj)
+    json.loads(json_object)
+    my_file.write(json_object)
