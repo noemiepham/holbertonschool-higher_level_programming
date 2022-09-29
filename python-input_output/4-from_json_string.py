df2753f273ccad9009acdaf24974f34b0db919e7
noemiepham <huynhtieuquy@gmail.com>
2022-09-29 14:21:05 +0200

    Reformat file

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """import module json"""
 import json
+
+
 def from_json_string(my_str):
-    """return object json"""
-    return json.loads(my_str)
+  """return object json"""
+  return json.loads(my_str)
