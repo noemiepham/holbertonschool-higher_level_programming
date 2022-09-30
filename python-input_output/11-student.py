a459ab74989734a74b5b1b865cead80977b057eb
noemiepham <huynhtieuquy@gmail.com>
2022-09-30 15:47:19 +0200

    replace attribute

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""student Json"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """conductor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary representation of a Student instance"""
+        if type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__:
+                    temp[elem] = self.__dict__[elem]
+            return temp
+
+    def reload_from_json(self, json):
+        """replace json"""
+        for key, value in json.items():
+            setattr(self, key, value)
