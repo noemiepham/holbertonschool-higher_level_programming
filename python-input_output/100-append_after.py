cfcd2132515d0ab4f5acef1bdc3ad13707b0c315
noemiepham <huynhtieuquy@gmail.com>
2022-10-01 08:17:51 +0200

    add new line with insert()

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -4,9 +4,13 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """append line after search"""
-    with open(filename, "w+", encoding="UTF8") as my_file:
-        for i in my_file.readlines():
-            print(i)
-            if search_string in i:
-                print(search_string)
-
+    with open(filename, "r+", encoding="UTF8") as read_file:
+        temp = read_file.readlines()
+    count = 0
+    with open(filename, "w", encoding="UTF8") as write_file:
+        for lines in temp:
+            count += 1
+            if search_string in lines:
+                temp.insert(count, new_string)
+        for lines in temp:
+            write_file.write(lines)
