df78e709b3a0712f7cdf0fd87cb02054c5cfa0e4
noemiepham <huynhtieuquy@gmail.com>
2022-10-03 07:49:54 +0200

    Super()id

--- python-almost_a_circle/README.md
+++ python-almost_a_circle/README.md
@@ -6,3 +6,6 @@ Python - Almost a circle
 #### What is *args and how to use it
 #### What is **kwargs and how to use it
 #### How to handle named arguments in a function
+#### Why private attributes with getter/setter? Why not directly public attribute?
+
+Because we want to protect attributes of our class. With a setter, you are able to validate what a developer is trying to assign to a variable. So after, in your class you can “trust” these attributes.
---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Inheritance de Base"""
+
+    def __init__(self, width=0, height=0, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @x.setter
+    def y(self, value):
+        self.__y = value
