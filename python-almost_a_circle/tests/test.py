592d866617d24e0d98bfee4cfdeea7a2abac4eca
noemiepham <huynhtieuquy@gmail.com>
2022-10-03 22:12:12 +0200

    test instance

--- python-almost_a_circle/tests/test.py
+++ python-almost_a_circle/tests/test.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python3
-"""import unitest"""
+"Unit tests for Base class"
 import unittest
-
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-  """Test Base"""
-  def test_cound_object(self):
-      """test couting number of"""
-      Base._Base__nb_object = 0
-      self.assertEqual(Base._Base__nb_objects, 0)
+    """Unit tests suite for Base class"""
+
+    def test_constantId(self):
+        """Test of Base for correctly initializing an id"""
+        b = Base(5)
+        self.assertEqual(b.id, 5)
+
+    def test_autoId(self):
+        """Test of Base for automatically assigning and incrementing an id"""
+        b = Base()
+        self.assertEqual(b.id, 1)
+
+    def test_float(self):
+        """Test of Base for input float"""
+        b = Base(9.5)
+        self.assertEqual(b.id, 9.5)
+
+    def test_float_inf(self):
+        """Test of Base for input case float infinity"""
+        b = Base(float('inf'))
+        self.assertEqual((b.id, float('inf')))
+
+    def test_string(self):
+        """Test of Base for case input is string"""
+        b = Base("string")
+        self.assertEqual(b.id, "string")
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 
