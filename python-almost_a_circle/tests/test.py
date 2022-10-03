284ce5068501369ad121b90bfafc255c5b4fac1f
noemiepham <huynhtieuquy@gmail.com>
2022-10-03 07:55:51 +0200

    test file

---
+++ python-almost_a_circle/tests/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""import unitest"""
+import unittest
+
+from models.base import Base
+
+
+class TestMaxInteger(unittest.TestCase):
+  """Test for “max at the end” list order"""
+
+
