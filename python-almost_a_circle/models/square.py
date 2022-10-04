9ac79c4bc6d8fb3d99425e01e323e12bf722cc94
noemiepham <huynhtieuquy@gmail.com>
2022-10-04 14:43:18 +0200

    test case error str

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,7 +24,5 @@ class Square(Rectangle):
 
     def __str__(self):
         """Return  value"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
-                                                         self.x, self.y, self.width)
-
-
+        return "[Square] ({:d}) {:d}/{:d} - " \
+               "{:d}".format(self.id,self.x, self.y, self.width)
