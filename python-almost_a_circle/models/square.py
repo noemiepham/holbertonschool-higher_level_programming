4beb14bf108189f314d8fa4197c8e76de094cd7f
noemiepham <huynhtieuquy@gmail.com>
2022-10-04 14:33:04 +0200

    git add square update

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""import module Rectangle"""
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class inheritance de Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """inherited of Rectangle"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """size needs to be an int"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return  value"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d})".format(self.id, self.x, self.y, self.width)
