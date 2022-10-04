fb4ad2939b114589c1236edae4c272505eb3de97
noemiepham <huynhtieuquy@gmail.com>
2022-10-04 15:50:59 +0200

    Square update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -29,18 +29,18 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update args"""
-
-    def update(self, *args, **kwargs):
         if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
+          for item, value in kwargs.items():
+             setattr(self, item, value)
         elif len(args) != 0:
-            try:
-                self.id = args[0]
-                self.size = args[1]
-                self.x = args[2]
-                self.y = args[3]
-            except IndexError:
-                pass
+          try:
+              self.id = args[0]
+              self.size = args[1]
+              self.x = args[2]
+              self.y = args[3]
+          except IndexError:
+              pass
         else:
-            print()
+          print()
+
+
