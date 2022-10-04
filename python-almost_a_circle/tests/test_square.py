ece33051039a9be03bfcf1b4c01b4fd74503fd25
noemiepham <huynhtieuquy@gmail.com>
2022-10-04 21:49:59 +0200

    Update test

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -2,7 +2,7 @@
 "Unit tests for Rectangle class"
 import unittest
 from unittest import mock
-from io import StringIO
+import io
 from models.square import Square
 
 
@@ -12,6 +12,34 @@ class TestSquare(unittest.TestCase):
       """test input size correct standard """
       s = Square(5)
       self.assertEqual(s.size, 5)
+  def test_area(self):
+      """testing area"""
+
+      s = Square(5)
+      self.assertEqual(s.area(), 25)
+
+      s = Square(1, 2)
+      self.assertEqual(s.area(), 1)
+  def test_display(self):
+      """test display()"""
+
+      s = Square(5)
+      with mock.patch("sys.stdout", new=io.StringIO()) as mock_stdout:
+        s.display()
+
+      assert mock_stdout.getvalue() == "#####\n#####\n#####\n#####\n#####\n"
+
+      s = Square(1, 2)
+      with mock.patch("sys.stdout", new=io.StringIO()) as mock_stdout:
+        s.display()
+
+      assert mock_stdout.getvalue() == "  #\n"
+  def test_string(self):
+      """Test str"""
+
+      s = Square(1, 2)
+      self.assertEqual(s.__str__(), '[Square] (11) 2/0 - 1')
 
 
-
+if __name__ == "__main__":
+    unittest.main()
