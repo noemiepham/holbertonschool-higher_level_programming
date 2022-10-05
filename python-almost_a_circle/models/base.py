2a10eafe13987caa50718c9890fe29e116d3133e
noemiepham <huynhtieuquy@gmail.com>
2022-10-05 07:02:57 +0200

    staticmethod

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,11 +14,17 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """JSON"""
-        temp = {}
         if list_dictionaries is None:
             return "[]"
         else:
             json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_str):
+        if type(json_str) != str:
+            return []
+        return json.load(json_str)
 
