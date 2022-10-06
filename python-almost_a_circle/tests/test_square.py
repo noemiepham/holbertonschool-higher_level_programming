dbf8848a4a6651a1aade54a1c0a621038fc34e10
noemiepham <huynhtieuquy@gmail.com>
2022-10-06 13:53:41 +0200

    test case square(1)

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -117,13 +117,19 @@ class TestSquare(unittest.TestCase):
         s = read_file.read()
         self.assertEqual(len(s), 39)
 
-    def test_save_to_file_empty(self):
+    def test_save_to_file_list_empty(self):
       """Test of Square.save_to_file([]) in Square exists"""
       Square.save_to_file([])
       with open("Square.json", mode="r") as read_file:
            s = read_file.read()
            self.assertEqual(s, "[]")
 
+    def test_save_to_file_empty(self):
+      """Test of Square.save_to_file([]) in Square exists"""
+      Square.save_to_file(None)
+      with open("Square.json", mode="r") as read_file:
+           s = read_file.read()
+           self.assertEqual(s, "[]")
     def test_save_to_file_none(self):
       """Test of Square.save_to_file([]) in Square exists"""
       with self.assertRaises(Exception):
