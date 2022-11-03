85c7609213478624b06dab5c55a522e4ab66c366
noemiepham <huynhtieuquy@gmail.com>
2022-11-03 13:07:35 +0100

    Show full Table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT first_table FROM hbtn_0c_0;
-SHOW TABLES;
+SHOW FULL TABLE;
