5229c848dae21ec3ef9f790183d6c845f891a391
noemiepham <huynhtieuquy@gmail.com>
2022-11-08 15:22:35 +0100

    add cites

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE  IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE 
+     IF NOT EXISTS `hbtn_0d_usa`.`cities`(
+          FOREIGN KEY(`state_id`),
+          PRIMARY KEY(`id`),
+          `id` INT      UNIQUE       NOT NULL     AUTO_INCREMENT,
+           `name` VARCHAR(256) NOT NULL
+     );
