dcea6de649e8dbda0c256382bbb44f7ec28d9430
noemiepham <huynhtieuquy@gmail.com>
2022-11-08 13:53:04 +0100

    Grant select user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,6 +5,6 @@ CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT 
-     ON `hbtn_0d_2`.* 
-     TO `user_0d_2`@`localhost`
+     ON hbtn_0d_2.* 
+     TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
