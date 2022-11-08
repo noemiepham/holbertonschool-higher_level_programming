6d9aaf374be1bc95332cb9200a3fbb121de058cd
noemiepham <huynhtieuquy@gmail.com>
2022-11-08 13:37:49 +0100

    create user select only

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@ CREATE DATABASE
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
+GRANT SELECT 
+     ON `hbtn_0d_2`.* 
+     TO `user_0d_2`@`localhost`
 FLUSH PRIVILEGES;
