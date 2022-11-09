a83f6129baec876c458ce5f1b9ba5d7cb29cc9d2
noemiepham <huynhtieuquy@gmail.com>
2022-11-09 08:48:34 +0100

    sorting order

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -7,4 +7,4 @@ FROM tv_shows AS ts
      LEFT JOIN tv_show_genres AS tsg ON (tsg.show_id = ts.id)
 
      LEFT JOIN tv_genres AS tg ON (tsg.genre_id = tg.id)
-ORDER BY ts.title ASC;
+ORDER BY ts.title, tg.name ASC;
