5b9a91d3e8d634280eabf3666e259faf29836269
noemiepham <huynhtieuquy@gmail.com>
2022-11-12 09:03:10 +0100

    corrige join

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,21 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
     cur = db.cursor()
-    cur.execute("SELECT ct.name\
+    cur.execute("SELECT *\
                          FROM cities AS ct \
                          INNER JOIN states AS st\
                          ON ct.state_id = st.id\
                          WHERE st.name = %s\
                          ORDER BY ct.id ", (sys.argv[4],))
-    query_rows = cur.fetchall()
-    results = []
-    for row in query_rows:
-        for field in row:
-            results.append(field)
-    cur.close()
-    db.close()
-    for i in range(len(results)):
-        if i == len(results) - 1:
-            print("{}".format(results[i]))
-        else:
-            print("{}, ".format(results[i]), end="")
+    print(", ".join([ct[2] for ct in cur.fetchall() if ct[4] == sys.argv[4]]))
