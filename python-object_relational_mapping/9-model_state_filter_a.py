36a2800c50b3101eafbc927e0dd63754ede9a1e8
noemiepham <huynhtieuquy@gmail.com>
2022-11-12 09:13:25 +0100

    add loop to fix error

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id)
-    if "a" in state.name:
-        print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
