231aa9db207937047a7cbb8859d5ce63da1f7987
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-05 15:22:40 +0100

    semistandard

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let string1 = 'C is fun';
-let string2 = 'Python is cool';
-let string3 = 'JavaScript is amazing';
+const string1 = 'C is fun';
+const string2 = 'Python is cool';
+const string3 = 'JavaScript is amazing';
 console.log(string1);
 console.log(string2);
 console.log(string3);
