4787026276a6126a58d086f41168c5373905b352
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-05 15:16:54 +0100

    update chmod

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+let string1 = 'C is fun';
+let string2 = 'Python is cool';
+let string3 = 'JavaScript is amazing';
+console.log(string1);
+console.log(string2);
+console.log(string3);
