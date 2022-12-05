a0983e6f77d7a7d2da4add3fe23214944327b8a6
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-05 20:59:12 +0100

    not a number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const args = process.argv;
 const rs = +args[2];
-if (rs === isNaN) {
-  console.log('Not a number');
+if (isNaN(rs)) {
+     console.log('Not a number');
 } else {
-  console.log(`My number: ${rs}`);
+     console.log(`My number: ${rs}`);
 }
