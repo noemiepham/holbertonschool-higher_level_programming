128cfb10a81fbb3201f8e7c0ec7cb6b0d6d8809e
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-05 22:10:12 +0100

    find biggets number

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const args = process.argv;
+let index = 0;
+let MaxNumber = 0;
+while (index in args) {
+  if (args[index] > MaxNumber) {
+    MaxNumber = args[index];
+  }
+  index++;
+}
+if (index === 2) {
+  console.log(0);
+} else if (index === 3) {
+  console.log(0);
+} else {
+  console.log(MaxNumber);
+}
