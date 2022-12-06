0479a6ce3d30b6233e20fcbebb15ada784d4bc17
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 13:36:14 +0100

    change condition

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,32 +1,5 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor (w, h) {
-    if (w >= 0 && h >= 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let row = 0; row < this.height; row++) {
-      for (let col = 0; col < this.width; col++) {
-        process.stdout.write('X');
-      }
-      console.log();
-    }
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-
-  rotate () {
-    this.width = this.height;
-    this.height = this.width;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
@@ -49,6 +22,4 @@ class Square extends Rectangle {
     this.size = this.size * 2;
   }
 }
-
-module.exports = Rectangle;
 module.exports = Square;
