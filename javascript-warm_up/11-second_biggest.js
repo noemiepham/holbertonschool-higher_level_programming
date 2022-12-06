bf04238a11e602bdd1431c9d6fa6da160493963f
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 07:40:18 +0100

    find second max number

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,33 @@
 #!/usr/bin/node
 
-const args = process.argv;
-let index = 0;
-let MaxNumber = 0;
-while (index in args) {
-  if (args[index] >= MaxNumber) {
-    MaxNumber = args[index - 1];
+function findSecondLargestElem (arr) {
+  let first = -1;
+  let second = -1;
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] > first) {
+      second = first;
+      first = arr[i];
+    } else if (arr[i] > second && arr[i] !== first) {
+      second = arr[i];
+    }
   }
+  console.log(second);
+}
+
+const args = process.argv;
+let index = 2;
+const table = [];
+
+while (index in args) {
+  table.push(+args[index]);
   index++;
 }
+
 if (index === 2) {
   console.log(0);
 } else if (index === 3) {
   console.log(0);
 } else {
-  console.log(MaxNumber);
+  findSecondLargestElem(table);
 }
