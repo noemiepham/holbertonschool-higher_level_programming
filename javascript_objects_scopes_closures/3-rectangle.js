5e51339b9a9ca6fefcc76ffd18afccf36cc94070
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 10:46:06 +0100

    update print rectangcle

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
-module.exports = class Rectangle {
-  // Class methods
+class Rectangle {
   constructor (w, h) {
-    if (w < 0 || h < 0) {
+    if (w && h && w >= 0 && h >= 0) {
       this.width = w;
       this.height = h;
     }
@@ -17,4 +15,5 @@ module.exports = class Rectangle {
       console.log();
     }
   }
-};
+}
+module.exports = Rectangle;
