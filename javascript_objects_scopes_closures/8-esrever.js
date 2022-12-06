2ff58337bd9694282d5bb0a1d850f88f269978ec
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 14:06:08 +0100

    Dont use reverse()

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return (list.reverse());
+  const table = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    table.push(list[i]);
+  }
+  return (table);
 };
