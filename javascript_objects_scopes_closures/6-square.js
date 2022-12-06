71f9e8d816b0fb47a063535ed0c01d7dcfaf9966
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 11:56:18 +0100

    print c = x

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,58 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size);
+    if (size >= 0) {
+      this.size = size;
+    }
+  }
+
+  charPrint (c) {
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        if (c === undefined) {
+          process.stdout.write('X');
+        } else {
+          process.stdout.write(c);
+        }
+      }
+      console.log();
+    }
+  }
+
+  double () {
+    this.size = this.size * 2;
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
