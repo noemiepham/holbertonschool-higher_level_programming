0d866e651ce1a5e8154b19d74faf79c19f8e14f6
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 13:48:46 +0100

    Change condition square

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -4,7 +4,7 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size);
-    if (size >= 0) {
+    if (size && size >= 0) {
       this.size = size;
     }
   }
