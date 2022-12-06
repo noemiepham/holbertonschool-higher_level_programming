b5e3739ddbb9eaecdd523618c6be021004e49c1b
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 11:08:30 +0100

    Double and rotate rectangcle

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,30 @@
 #!/usr/bin/node
+
 class Rectangle {
-     constructor (w, h) {
-          if (w && h && w >= 0 && h >= 0) {
-         this.width = w;
-         this.height = h;
-       }
-     }
-   
-     print () {
-       for (let row = 0; row < this.height; row++) {
-         for (let col = 0; col < this.width; col++) {
-           process.stdout.write('X');
-         }
-         console.log();
-       }
-     }
-     double () {
-          for (let row = 0; row < this.height * 2; row++) {
-               for (let col = 0; col < this.width * 2; col++) {
-                 process.stdout.write('X');
-               }
-               console.log();
-             }
-     }
-     rotate () {
-          this.height = this.width;
-          this.width = this.height;
-          this.print = print()
-     }
-   }
-   module.exports = Rectangle;
+  constructor (w, h) {
+    if (w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
+}
+module.exports = Rectangle;
