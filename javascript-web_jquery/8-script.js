b652a2e8853e652900041eb53575bd82beb11bd8
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-12 16:42:07 +0100

    update projeté

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+     $.each(data.results, function (key, value) {
+          $('UL#list_movies').append(`<li>${value.title}</li>`);
+     });
+});
