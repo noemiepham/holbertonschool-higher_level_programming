4b90c30a8617b668c922735844db8c46ee9c265d
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-13 07:49:54 +0100

    Count name acteur

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
-const URL = process.argv[2];
-request.get(URL, function (error, response, data) {
+const PathUrl = process.argv[2];
+// const PathUrl = `https://swapi-api.hbtn.io/api/films/films/}`;
+let counts = 0;
+request(PathUrl, function (error, request, data) {
   if (error) {
-    return console.log(error);
+    console.log(error);
   } else {
-     let count = 0;
-     for (let i in JSON.parse(data).results){
-          
-     }
+    for (const movie of JSON.parse(data).results) {
+      for (const character of movie.characters) {
+        if (character.includes('1')) {
+          counts++;
+        }
+      }
+    }
+    console.log(counts);
   }
 });
